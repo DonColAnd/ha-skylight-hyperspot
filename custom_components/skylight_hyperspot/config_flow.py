5bd203688168fd7a7a4583d3dfeef49422e7f46d
DonColAnd <153138521+DonColAnd@users.noreply.github.com>
2026-05-21 00:54:16 +0200

    Update config_flow.py

--- custom_components/skylight_hyperspot/config_flow.py
+++ custom_components/skylight_hyperspot/config_flow.py
@@ -1,52 +1,14 @@
-from __future__ import annotations
+if user_input is not None:
+    host = user_input["host"].strip()
+    name = user_input["name"].strip() or "Skylight Hyperspot"
 
-import voluptuous as vol
+    await self.async_set_unique_id(host)
+    self._abort_if_unique_id_configured()
 
-from homeassistant import config_entries
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
-from .api import SkylightApiError, SkylightHyperspotApi
-from .const import CONF_HOST, CONF_NAME, DOMAIN
-
-
-class SkylightConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        errors = {}
-
-        if user_input is not None:
-            host = user_input[CONF_HOST].strip()
-            name = user_input[CONF_NAME].strip() or "Skylight Hyperspot"
-
-            api = SkylightHyperspotApi(
-                host,
-                async_get_clientsession(self.hass),
-            )
-
-            try:
-                await api.model()
-            except SkylightApiError:
-                errors["base"] = "cannot_connect"
-            else:
-                await self.async_set_unique_id(host)
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(
-                    title=name,
-                    data={
-                        CONF_HOST: host,
-                        CONF_NAME: name,
-                    },
-                )
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_HOST): str,
-                    vol.Required(CONF_NAME, default="Skylight Hyperspot"): str,
-                }
-            ),
-            errors=errors,
-        )
+    return self.async_create_entry(
+        title=name,
+        data={
+            "host": host,
+            "name": name,
+        },
+    )
